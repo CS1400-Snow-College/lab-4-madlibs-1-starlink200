9bfdd1cd30f0abf2439ee36a758c1bb9b564a931
Caleb Roskelley <caleb.roskelley@students.snow.edu>
2024-09-20 01:02:38 -0600

    program accurately finds and asks users for most words

--- Program.cs
+++ Program.cs
@@ -1,21 +1,4 @@
-﻿/**************************************************************************************************
-* Madlibs is a game in which you take a story and mix up the word types.
-* You collect a bunch of words and then insert them into the story.
-*
-* For example:
-*
-* A vacation is when you take a trip to some (adjective) place with your
-* (adjective) family. Usually you go to some place that is near a/an (noun) or up on a/an (noun).
-*
-* You would then ask someone else for two nouns and two adjectives, they would say
-* 'mountain' , 'billy goat', 'ugly', and 'gentle'.   You'd insert these into the story becomes:
-
-* A vacation is when you take a trip to some ugly place with your gentle family. Usually you go to
-* some place that is near a/an mountain or up on a/an billy goat
-
-* Your job is to build a code that detects a word in parenthesis and asks the user for a word of that
-* type. You will print the original story and the new 'mad libbed' story.
-***************************************************************************************************/
+﻿
 Console.Clear();
 //Describe game to user
 Console.WriteLine("Hello! This is a game called madlibs which will ask you for some words like" +
@@ -31,15 +14,37 @@ string originalStory = "A vacation is when you take a trip to some (adjective) p
 + " (place) and I will practice (verb ending in “ing”) Parents need vacations more than kids because"
 + " parents are always very (adjective) and because they have to work (number) hours every day all "
 + "year making enough (plural noun) to pay for the vacation.";
+Console.Write(originalStory);
+Console.WriteLine("");
 //create an array that will hold each word as seperate items in an array
-string[] newStory = originalStory.Split();
+string[] newStory = originalStory.Split(" ");
 for(int i = 0; i < newStory.Length - 1; i++)
 {
     //initializing a string variable that will have the value of a specific item in the array
     //to be tested against
     string newStoryWord = newStory[i];
-    if(newStoryWord[0].Equals('('))
+    string lastStoryWord = newStory[0];
+    if(i >= 1)
     {
-        Console.WriteLine("found a (");
+        lastStoryWord = newStory[i-1];
     }
+    if(newStoryWord[0].Equals('(') && (newStoryWord[newStoryWord.Length - 1].Equals(')') || newStoryWord[newStoryWord.Length - 1].Equals('.')))
+    {
+        Console.WriteLine($"Please provide a {newStoryWord}");
+        string newWord = Console.ReadLine();
+        newStory[i] = newWord;
+        Console.Clear();
+    }
+    else if(((newStoryWord[newStoryWord.Length - 1].Equals(')') || newStoryWord[newStoryWord.Length - 1].Equals('.')) && !newStoryWord[0].Equals('(') && lastStoryWord[0].Equals('(')))
+    {
+        Console.Clear();
+        Console.WriteLine($"Please provide a {lastStoryWord} {newStoryWord}");
+        string newWord = Console.ReadLine();
+        newStory[i] = newWord;
+        newStory[i-1] = "";
+    }
+}
+foreach(string word in newStory)
+{
+    Console.Write($" {word}");
 }
